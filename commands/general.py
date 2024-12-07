49f050e11e479f9d98c8247396296f1ee430dc17
Suno <ilovesuno@proton.me>
2024-12-07 00:53:04 -0700

    Update general.py

--- commands/general.py
+++ commands/general.py
@@ -9,22 +9,6 @@ class General(Commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @Commands.cooldown(1, 10, Commands.BucketType.user)
-    @Commands.hybrid_command(name="buy",
-                             description="Sets everything up for you to buy Starry")
-    async def buy(self, CTX: Commands.Context):
-        guilds = OS.getenv("WHITELISTED", "").split(",")
-        
-        for id in guilds:
-            try:
-                if CTX.guild.id != int(id.strip()):
-                    return await CTX.reply(embed=errorEmbed(Discord.Color.red(),
-                                           "Message Purge ‎ <a:bonk:1289335115045928981>",
-                                           "You are not allowed to use this command in this server.\n- To use this app, join the Starry server!\n\nInvite: [disсord.gg/zyXZSn97hN](https://discord.gg/zyXZSn97hN)"))
-            
-            except ValueError:
-                continue
-
     @Commands.cooldown(1, 5, Commands.BucketType.user)
     @Commands.hybrid_command(name="script",
                              description="Get the latest script for Starry")
