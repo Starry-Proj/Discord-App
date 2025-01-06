9b9749cce0cec805fe93524ae80c16597039728f
Suno <ilovesuno@proton.me>
2025-01-05 23:56:33 -0700

    Added command + new function usage

--- commands/premium.py
+++ commands/premium.py
@@ -1,9 +1,10 @@
 import discord as Discord;
-import os      as OS;
 
-from discord.ext import commands as Commands;
-from assets.functions import *;               # Load our Functions
-from assets.constants import *;               # Load our Constant Variables
+from discord.ui       import Button, View;
+from discord.ext      import commands as Commands;  
+from discord.ext      import commands as Commands;
+from assets.functions import *;                     # Load our Functions
+from assets.constants import *;                     # Load our Constant Variables
 
 """
 
@@ -21,10 +22,39 @@ class Premium(Commands.Cog):
     def __init__(self, App) -> None:
         self.App = App
 
-
     # Commands
 
-    # ...
+    # -- Buy Command -- #
+
+    @Commands.cooldown(1, 5, Commands.BucketType.user)
+    @Commands.hybrid_command(name="buy",
+                             description="Link the channel to purchase Starry premium")
+    
+    async def Buy(self, CTX: Commands.Context):
+        """
+        
+            In short, this command will just be an advertisement lol,
+
+            .. just hope the user actually buys Starry Premium!
+        
+        """
+
+        Description: str = OpenFile("premium.md", "r")
+
+        Embed: Discord.Embed = Discord.Embed(title="Starry Premium",
+                                             description=Description,
+                                             color=Discord.Color.green())
+        
+        NewViewer: View = View()
+
+        ViewGithub: Button = Button(label="View Webpage", style=Discord.ButtonStyle.link, url="https://starry.luau.tech/")
+
+        ButtonList: list = [ViewGithub]
+
+        for Item in ButtonList:
+            NewViewer.add_item(Item)
+
+        await CTX.reply(embed=Embed, view=NewViewer, ephemeral=True)
 
 
 # -- Setup the Cog -- #
